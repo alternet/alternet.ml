d39cd2e8f9d1c5243f4499a232e6824cde25f34c
ppoulard <philippe.poulard@inria.fr>
2017-07-13 21:05:51 +0200

    Clean

--- tools/src/main/java/ml/alternet/util/StringBuilderUtil.java
+++ tools/src/main/java/ml/alternet/util/StringBuilderUtil.java
@@ -1,5 +1,6 @@
 package ml.alternet.util;
 
+import java.util.function.Consumer;
 import java.util.stream.Collector;
 
 /**
@@ -13,17 +14,22 @@ public class StringBuilderUtil {
     /**
      * Return a collector that collects items inside an existing buffer.
      *
-     * @param delimiter The delimiter.
      * @param prefix The prefix.
+     * @param delimiter The delimiter.
      * @param suffix The suffix.
-     * @param buf The buffer.
+     * @param buffer The buffer.
+     * @param adder A function that adds its T argument to the buffer.
+     *
      * @return A collector to a buffer.
+     *
+     * @param <T> The type of the items to collect.
      */
-    public static Collector<String, StringBuilder, StringBuilder> collectorOf(
-            CharSequence delimiter,
+    public static <T> Collector<T, StringBuilder, StringBuilder> collectorOf(
             CharSequence prefix,
+            CharSequence delimiter,
             CharSequence suffix,
-            StringBuilder buf)
+            StringBuilder buffer,
+            Consumer<T> adder)
     {
         Object separator = new Object() {
             boolean first = true;
@@ -36,11 +42,33 @@ public class StringBuilderUtil {
             }
         };
         return Collector.of(
-            () -> buf.append(prefix),
-            (sb, s) -> sb.append(separator).append(s),
+            () -> buffer.append(prefix),
+            (sbuf, item) -> {
+                sbuf.append(separator);
+                adder.accept(item);
+            },
             StringBuilder::append,
             sb -> sb.append(suffix)
         );
+    }
+
+    /**
+     * Return a collector that collects items inside an existing buffer.
+     *
+     * @param prefix The prefix.
+     * @param delimiter The delimiter.
+     * @param suffix The suffix.
+     * @param buffer The buffer.
+     *
+     * @return A collector to a buffer.
+     */
+    public static Collector<String, StringBuilder, StringBuilder> collectorOf(
+            CharSequence prefix,
+            CharSequence delimiter,
+            CharSequence suffix,
+            StringBuilder buffer)
+    {
+        return collectorOf(prefix, delimiter, suffix, buffer, s -> buffer.append(s));
     }
 
 }
