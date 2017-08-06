9f545b9d9d6e19e1cd9ca5bdf8900f2ccc173564
ppoulard <philippe.poulard@inria.fr>
2017-08-06 15:20:50 +0200

    Move to own module

--- security/src/main/java/ml/alternet/security/auth/formats/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains common built-in family crypt formats that
- * allow to extract the hash scheme used for resolving the concrete hasher.
- *
- * <p><strong>It is up to the user to register it to the discovery service</strong>.</p>
- */
-@ml.alternet.misc.InfoClass
-package ml.alternet.security.auth.formats;
