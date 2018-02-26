86f3530d25bc98e129d3ce9835bdaaa96597d68d
ppoulard <philippe.poulard@inria.fr>
2018-02-26 10:45:42 +0100

    Nothing intersting

--- security-auth/src/main/java/ml/alternet/security/auth/formats/package-info.java
+++ security-auth/src/main/java/ml/alternet/security/auth/formats/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Common built-in crypt formats and algorithms used to retrieve
- * concrete hashers.
+ * Common built-in crypt formats used to retrieve concrete hashers.
  */
 @ml.alternet.misc.InfoClass
 package ml.alternet.security.auth.formats;
